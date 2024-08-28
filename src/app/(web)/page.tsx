18891ceba87d6b57a06e720a275a3658987e0334
snsa-kscc <sinisa@dvasadva.com>
2024-08-28 12:09:26 +0200

    add framer motion

--- src/app/(web)/page.tsx
+++ src/app/(web)/page.tsx
@@ -1,7 +1,13 @@
+'use client'
+
+import { motion } from 'framer-motion'
+
 export default function Home() {
   return (
     <main>
-      <h1>Hello World</h1>
+      <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 5 }}>
+        <h1>Hello World</h1>
+      </motion.div>
     </main>
   )
 }

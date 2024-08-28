e56e8ae5a421e8ec2762167c92e073ac25e854c1
snsa-kscc <sinisa@dvasadva.com>
2024-08-28 17:00:02 +0200

    add anima deps

--- src/app/(web)/page.tsx
+++ src/app/(web)/page.tsx
@@ -1,13 +1,24 @@
 'use client'
 
 import { motion } from 'framer-motion'
+import gsap from 'gsap'
+import { useGSAP } from '@gsap/react'
+
+gsap.registerPlugin(useGSAP)
 
 export default function Home() {
+  useGSAP(() => {
+    gsap.to('.box', { x: 360 })
+  })
+
   return (
     <main>
       <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 5 }}>
         <h1>Hello World</h1>
       </motion.div>
+      <div>
+        <p className="box">ja sam manupuliraan od gsapa</p>
+      </div>
     </main>
   )
 }

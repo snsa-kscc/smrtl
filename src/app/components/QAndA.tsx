9c28bdffcd9323ec9bca154a0c735bece2decfd0
snsa-kscc <sinisa@dvasadva.com>
2024-09-04 17:17:51 +0200

    add qa styles

--- src/app/components/QAndA.tsx
+++ src/app/components/QAndA.tsx
@@ -2,6 +2,7 @@
 
 import React from 'react'
 import { Accordion, AccordionItem, AccordionTrigger, AccordionContent } from './ui/accordion'
+import { QAShape } from './shapes/QAShape'
 
 export function QAndA({
   title,
@@ -13,20 +14,30 @@ export function QAndA({
   qAndABox: { question: string; answer: string }[]
 }) {
   return (
-    <div className="w-full px-4 pt-16">
-      <h2 className="mb-2 text-2xl font-bold">{title}</h2>
-      <p className="mb-6">{subtitle}</p>
-      <div className="mx-auto w-full max-w-md rounded-2xl bg-white p-2">
-        <Accordion type="single" collapsible>
-          {qAndABox.map((item, index) => (
-            <AccordionItem key={index} value={item.question}>
-              <AccordionTrigger className="text-lg font-bold hover:no-underline">
-                {item.question}
-              </AccordionTrigger>
-              <AccordionContent>{item.answer}</AccordionContent>
-            </AccordionItem>
-          ))}
-        </Accordion>
+    <div className="bg-smartellDarkBlue">
+      <h2 className="mb-2 text-balance px-10 pt-20 text-center text-10xl font-bold text-smartellLightPurple">
+        {title}
+      </h2>
+      <p className="mx-auto w-1/2 text-white">{subtitle}</p>
+      <div className="flex items-center justify-between gap-10">
+        <div className="relative max-w-2xl basis-1/2 translate-y-40">
+          <div className="absolute left-1/2 top-[10%] -translate-x-1/2 text-10xl font-bold text-smartellLightPurple">
+            <p>QA</p>
+          </div>
+          <QAShape />
+        </div>
+        <div className="mx-32 max-w-3xl basis-1/2">
+          <Accordion type="single" collapsible>
+            {qAndABox.map((item, index) => (
+              <AccordionItem key={index} value={item.question} className="my-8">
+                <AccordionTrigger className="text-left text-lg font-bold text-white hover:no-underline">
+                  {item.question}
+                </AccordionTrigger>
+                <AccordionContent className="w-11/12 text-white">{item.answer}</AccordionContent>
+              </AccordionItem>
+            ))}
+          </Accordion>
+        </div>
       </div>
     </div>
   )
---
+++ src/app/components/shapes/QAShape.tsx
@@ -0,0 +1,17 @@
+export function QAShape() {
+  return (
+    <svg
+      width="100%"
+      height="100%"
+      viewBox="0 0 603 1044"
+      fill="none"
+      preserveAspectRatio="xMidYMid meet"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M0 700.835L603 1044V328.682L0 0V700.835Z" fill="#E8EDF2" />
+      <path d="M397 356H124V522H397V356Z" stroke="#5E5EFF" stroke-width="14" />
+      <path d="M301 426H446" stroke="#021F53" stroke-width="14" />
+      <path d="M301 458H446" stroke="#5E5EFF" stroke-width="14" />
+    </svg>
+  )
+}

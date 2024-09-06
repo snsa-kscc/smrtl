f3ccc2ad100399d60a4c63fa67b23a11c5ecceb1
snsa-kscc <sinisa@dvasadva.com>
2024-09-07 00:38:06 +0200

    add image hero styles

--- src/app/components/ImageHero.tsx
+++ src/app/components/ImageHero.tsx
@@ -1,22 +1,29 @@
 'use client'
 
-import Image from 'next/image'
 import { ImageShape } from './shapes/ImageShape'
 
 type ImageHeroProps = {
   title: string
   description: string
-  image: { url: string; alt: string }
+  image: { url: string; alt: string; filename: string }
   logo: boolean
 }
 
 export function ImageHero({ title, description, image, logo }: ImageHeroProps) {
   return (
-    <div className="flex flex-col items-center justify-center">
-      <h2 className="text-4xl font-bold">{title}</h2>
-      <p className="text-lg">{description}</p>
-      <Image src={image.url} alt={image.alt} width={1000} height={1000} />
-      {logo && <ImageShape />}
+    <div
+      className="grid min-h-[70vh] grid-cols-3 grid-rows-2 items-end justify-items-start gap-y-10 bg-cover bg-center p-20"
+      style={{
+        backgroundImage: `url('${image.url}')`,
+      }}
+    >
+      <h2 className="col-span-2 text-7xl font-bold text-white">{title}</h2>
+      <p className="row-start-2 self-start text-lg text-white">{description}</p>
+      {logo && (
+        <div className="col-start-3 row-start-2 translate-y-10 scale-150">
+          <ImageShape />
+        </div>
+      )}
     </div>
   )
 }

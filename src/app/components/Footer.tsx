8d69fb4ee17f643f3bdd2a6011b33f8059a3b3fb
snsa-kscc <sinisa@dvasadva.com>
2024-09-21 03:17:01 +0200

    add footer links

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,13 +1,40 @@
+import Link from 'next/link'
 import { getGlobal } from '../lib/getGlobals'
-import type { Locale } from 'i18n.config'
+import configPromise from '@payload-config'
+import { getPayloadHMR } from '@payloadcms/next/utilities'
+import { Locale, i18n } from 'i18n.config'
 import type { Footer } from '../../payload-types'
 
 export async function Footer({ lang }: { lang: Locale }) {
-  const footer = (await getGlobal('footer')) as Footer
+  const footer = (await getGlobal('footer', undefined, lang)) as Footer
+
+  const payload = await getPayloadHMR({ config: configPromise })
+
+  const result = await payload.find({
+    collection: 'pages',
+    depth: 1,
+    locale: lang,
+  })
+
+  const foterLinks = result.docs
+    .filter((doc) => doc.slug !== 'home')
+    .map((doc) => ({
+      label: doc.title,
+      href: i18n.defaultLocale === lang ? `/${doc.slug}` : `/${lang}/${doc.slug}`,
+    }))
 
   return (
-    <div className="bg-smartellDarkBlue px-20 py-8 text-smartellLightPurple">
-      {footer.copyright}
-    </div>
+    <>
+      <div className="bg-smartellDarkBlue px-20 py-8 text-smartellLightPurple">
+        {footer.copyright}
+      </div>
+      <div className="p-20">
+        {foterLinks.map((link, index) => (
+          <div key={index}>
+            <Link href={link.href}>{link.label.toUpperCase()}</Link>
+          </div>
+        ))}
+      </div>
+    </>
   )
 }

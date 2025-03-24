ebf1768f7b7e6e0dea5cc9f749f9a9ef1bb6ce8f
snsa-kscc <sinisa@dvasadva.com>
2025-03-24 19:36:36 +0100

    add brands grid

--- src/app/components/Brands.tsx
+++ src/app/components/Brands.tsx
@@ -16,24 +16,55 @@ type BrandsProps = {
 
 export function Brands({ title, description, brands }: BrandsProps) {
   return (
-    <div className="bg-smartellDarkBlue py-40">
-      <h3 className="text-smartellLightPurple text-center text-4xl font-bold text-balance lg:text-5xl xl:text-7xl">
+    <div className="bg-smartellDarkBlue py-20">
+      <h3 className="text-smartellLightPurple text-center text-3xl font-bold text-balance lg:text-4xl xl:text-5xl">
         {title}
       </h3>
-      <p className="mx-auto w-2/3 pt-10 text-center text-4xl font-bold text-balance text-white lg:text-5xl xl:text-6xl">
+      <p className="mx-auto w-2/3 pt-6 text-center text-3xl font-bold text-balance text-white lg:text-4xl xl:text-5xl">
         {description}
       </p>
-      <div className="my-20 flex items-center justify-center gap-6">
-        {brands.map((brand) => (
-          <div key={brand.image.id} className="basis-12">
-            <Image
-              src={brand.image.url}
-              alt={brand.image.alt}
-              width={brand.image.width}
-              height={brand.image.height}
-            />
-          </div>
-        ))}
+      <div className="mx-auto my-12 flex max-w-4xl flex-col gap-8 px-4">
+        <div className="flex justify-evenly">
+          {brands.slice(0, 3).map((brand, index) => (
+            <div key={index} className="flex items-center">
+              <Image
+                src={brand.image.url}
+                alt={brand.image.alt}
+                width={brand.image.width}
+                height={brand.image.height}
+                className="w-full max-w-[120px]"
+              />
+            </div>
+          ))}
+        </div>
+
+        <div className="flex justify-evenly">
+          {brands.slice(3, 5).map((brand, index) => (
+            <div key={index + 3} className="flex items-center">
+              <Image
+                src={brand.image.url}
+                alt={brand.image.alt}
+                width={brand.image.width}
+                height={brand.image.height}
+                className="w-full max-w-[120px]"
+              />
+            </div>
+          ))}
+        </div>
+
+        <div className="flex justify-center">
+          {brands.slice(5, 6).map((brand, index) => (
+            <div key={index + 5} className="flex items-center">
+              <Image
+                src={brand.image.url}
+                alt={brand.image.alt}
+                width={brand.image.width}
+                height={brand.image.height}
+                className="w-full max-w-[120px]"
+              />
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   )
